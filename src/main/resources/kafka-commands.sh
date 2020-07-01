41c834ad9ec0ad85590e5fda7487a056c3b7e27c
vinodhinic <cheeky-chinnu>
2020-07-02 02:25:42 +0530

    Compaction does not work in windows.
    I moved the entire setup to linux and hence the command update.

--- src/main/resources/kafka-commands.sh
+++ src/main/resources/kafka-commands.sh
@@ -1,14 +1,18 @@
 
-bin\windows\kafka-topics.bat --create --zookeeper localhost:2181 --replication-factor 1 --partitions 1 --topic events
+bin/zookeeper-server-start.sh config/zookeeper.properties
 
-bin\windows\kafka-topics.bat --create --zookeeper localhost:2181 --replication-factor 1 --partitions 1 --topic events-upper
+bin/kafka-server-start.sh config/server.properties
 
-bin\windows\kafka-topics.bat --zookeeper localhost:2181 --list
+bin/kafka-topics.sh --create --zookeeper localhost:2181 --replication-factor 1 --partitions 1 --topic events
 
-bin\windows\kafka-consumer-groups.bat --bootstrap-server localhost:9092 --all-groups --describe
+bin/kafka-topics.sh --create --zookeeper localhost:2181 --replication-factor 1 --partitions 1 --topic events-upper
 
-bin\windows\kafka-console-consumer.bat --bootstrap-server localhost:9092 --topic events --from-beginning --formatter kafka.tools.DefaultMessageFormatter --property print.key=true --property print.value=true --property key.deserializer=org.apache.kafka.common.serialization.StringDeserializer --property value.deserializer=org.apache.kafka.common.serialization.StringDeserializer
+bin/kafka-topics.sh --zookeeper localhost:2181 --list
 
-bin\windows\kafka-console-consumer.bat --bootstrap-server localhost:9092 --topic events-upper --from-beginning --formatter kafka.tools.DefaultMessageFormatter --property print.key=true --property print.value=true --property key.deserializer=org.apache.kafka.common.serialization.StringDeserializer --property value.deserializer=org.apache.kafka.common.serialization.StringDeserializer
+bin/kafka-consumer-groups.sh --bootstrap-server localhost:9092 --all-groups --describe
 
-bin\windows\kafka-console-consumer.bat --bootstrap-server localhost:9092 --topic poc-app-poc-state-store-changelog --from-beginning --formatter kafka.tools.DefaultMessageFormatter --property print.key=true --property print.value=true --property key.deserializer=org.apache.kafka.common.serialization.StringDeserializer --property value.deserializer=org.apache.kafka.common.serialization.StringDeserializer
+bin/kafka-console-consumer.sh --bootstrap-server localhost:9092 --topic events --from-beginning --formatter kafka.tools.DefaultMessageFormatter --property print.key=true --property print.value=true --property key.deserializer=org.apache.kafka.common.serialization.StringDeserializer --property value.deserializer=org.apache.kafka.common.serialization.StringDeserializer
+
+bin/kafka-console-consumer.sh --bootstrap-server localhost:9092 --topic events-upper --from-beginning --formatter kafka.tools.DefaultMessageFormatter --property print.key=true --property print.value=true --property key.deserializer=org.apache.kafka.common.serialization.StringDeserializer --property value.deserializer=org.apache.kafka.common.serialization.StringDeserializer
+
+bin/kafka-console-consumer.sh --bootstrap-server localhost:9092 --topic sort-app-sort-state-store-changelog --from-beginning --formatter kafka.tools.DefaultMessageFormatter --property print.key=true --property print.value=true --property key.deserializer=org.apache.kafka.common.serialization.StringDeserializer --property value.deserializer=org.apache.kafka.common.serialization.StringDeserializer
